a637c088ae1f73a783eb0f57053c1a852b18e426
Frederick <frederickadigun@gmail.com>
2025-08-19 01:34:32 +0000

    chore: add Terraform config + site folder

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "site_url" {
-  description = "Live Netlify site URL"
-  value       = netlify_site.site.ssl_url
+  value       = data.netlify_site.existing_site.ssl_url
+  description = "The URL of the existing Netlify site"
 }
