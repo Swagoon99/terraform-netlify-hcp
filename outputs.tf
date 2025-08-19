18e79ad3368130f4ef6f8f1d8950d11fb4dd8290
Frederick <frederickadigun@gmail.com>
2025-08-19 17:45:29 +0000

    chore: add Terraform config + site folder

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "site_id" {
   value       = data.netlify_site.mynetlifyhug.id
 }
 
-output "site_url" {
-  description = "The live Netlify site URL"
-  value       = data.netlify_site.mynetlifyhug.url
-}
-
 output "site_name" {
   description = "The Netlify Site Name"
   value       = data.netlify_site.mynetlifyhug.name
