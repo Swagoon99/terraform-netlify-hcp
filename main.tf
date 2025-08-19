4900c7717ccec9fa1767b64337ac7811d8451a6a
Frederick <frederickadigun@gmail.com>
2025-08-19 01:44:51 +0000

    chore: add Terraform config + site folder

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
+# Provider configuration
 provider "netlify" {
-  token = var.netlify_token != "" ? var.netlify_token : getenv("NETLIFY_TOKEN")
+  token = var.netlify_token  # Set via environment variable if left empty
 }
 
 # Reference an existing Netlify site
 data "netlify_site" "existing_site" {
-  name = "mynetlifyhug"  # Replace with the actual site name you created in Netlify
+  name = "mynetlifyhug"  # Replace with the exact name of your site in Netlify
 }
