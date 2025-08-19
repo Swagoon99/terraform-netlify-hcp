851e75702e7abf5cdad8b6746b0b854360aad541
Frederick <frederickadigun@gmail.com>
2025-08-19 01:31:19 +0000

    chore: add Terraform config + site folder

--- main.tf
+++ main.tf
@@ -2,12 +2,7 @@ provider "netlify" {
   token = var.netlify_token != "" ? var.netlify_token : getenv("NETLIFY_TOKEN")
 }
 
-resource "netlify_site" "site" {
-  name          = "hug-ibadan-demo"
-  custom_domain = ""  # Optional: Set your custom domain here
-}
-
-resource "netlify_site_deploy" "deploy" {
-  site_id = netlify_site.site.id
-  dir     = "${path.module}/site"
+# Reference an existing Netlify site
+data "netlify_site" "existing_site" {
+  name = "mynetlifyhug"  # Replace with the actual site name you created in Netlify
 }
