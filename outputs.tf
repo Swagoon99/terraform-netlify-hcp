3bcbdbf0f75a77482fe771de61dbaca4f680fe09
Frederick <frederickadigun@gmail.com>
2025-08-19 01:41:38 +0000

    chore: add Terraform config + site folder

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "site_url" {
-  value       = data.netlify_site.existing_site.url
+  value       = "https://${data.netlify_site.existing_site.name}.netlify.app"
   description = "The URL of the existing Netlify site"
 }
