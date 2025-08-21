120586569f18c12ecfe4080769905815e3eab227
Frederick Adigun <frederickadigun@gmail.com>
2025-08-21 19:41:41 +0100

    Deleted backend.tf to sync with local state

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "Swagoon"  # Replace with your HCP Terraform org name
-
-    workspaces {
-      name = "terraform-netlify-hcp"  # Replace with your workspace name
-    }
-  }
-}
