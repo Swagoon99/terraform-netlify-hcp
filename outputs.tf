02635ca0f85b646778ccbcec0d4d4d3016c3a5a0
Frederick Adigun <frederickadigun@gmail.com>
2025-08-21 15:37:20 +0100

    Fix outputs to handle empty domain_aliases

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output "site_name" {
   value       = data.netlify_site.site.name
 }
 
-output "site_url" {
-  description = "The default Netlify URL (HTTPS)"
-  value       = data.netlify_site.site.url
-}
-
 output "site_custom_domain" {
   description = "Custom domain if configured, otherwise null"
   value       = length(data.netlify_site.site.custom_domain) > 0 ? data.netlify_site.site.custom_domain : null
+}
+
+output "site_domain_aliases" {
+  description = "Domain aliases attached to the site"
+  value       = data.netlify_site.site.domain_aliases
 }
